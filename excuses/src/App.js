61606d40b9acd829b4e58a60e62fa39ca81f327e
Carlos Zalpa <nedhel.90@gmail.com>
2022-10-25 11:21:34 -0500

    Finish it

--- excuses/src/App.js
+++ excuses/src/App.js
@@ -1,20 +1,53 @@
+import React, { useState, useEffect } from "react";
 import { Excuses } from "./Excuses";
 
 function App() {
+    const [excuses, setExcuses] = useState();
+    const [input, setInput] = useState(0);
+    const [numberExcuses, setNumberExcuses] = useState(0);
+    useEffect(() => {
+        fetch(`https://excuser.herokuapp.com/v1/excuse/${input}`)
+            .then((response) => response.json())
+            .then((json) => {
+                console.log(json);
+                setExcuses(json);
+            })
+            .catch((error) => console.log(error));
+    }, [numberExcuses]);
+
+    function handleOnChangeInput(e) {
+        setInput(e.target.value);
+    }
+
+    function handleClick() {
+        setNumberExcuses(input);
+    }
+
     return (
         <div className="h-screen">
             <div className="flex flex-col bg-slate-800 justify-around items-center h-1/3">
                 <h1 className="text-3xl text-white font-bold">
                     Escribe el numero de Excusas que deseas solicitar
                 </h1>
-                <input type="number" className="rounded-md " />
+                <input
+                    type="number"
+                    className="rounded-md"
+                    value={input}
+                    onChange={handleOnChangeInput}
+                />
                 <input
                     type="button"
                     value="Solicitar"
                     className="rounded-md bg-green-300 p-3"
+                    onClick={handleClick}
                 />
             </div>
-            <div className="h-2/3 bg-gray-900"></div>
+            <div className="h-2/3 bg-gray-900">
+                {numberExcuses > 0 &&
+                    excuses.map((excuse) => {
+                        return <Excuses key={excuse.id} excuse={excuse} />;
+                    })}
+            </div>
         </div>
     );
 }
--- excuses/src/Excuses.jsx
+++ excuses/src/Excuses.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export const Excuses = ({excuse})=>{
     return(
-        <div>
-            <h1>Excusa # {excuse.id}</h1>
-            <p>{excuse.excuse}</p>
-            <h2>Se puede aplicar en {excuse.category}</h2>
+        <div className="flex flex-col items-center border border-white border-4 p-2 mb-3">
+            <h1 className="text-xl font-bold text-white">Excusa # {excuse.id}</h1>
+            <p className="text-orange-600">{excuse.excuse}</p>
+            <h2 className="text-white font-semibold">Se puede aplicar en {excuse.category}</h2>
         </div>
     )
 }
